69d01529192f5b407aea0a70144fd7889f243ab9
xu-jerry <jerryxu4@gmail.com>
2021-02-21 17:55:08 -0800

    Added two php challenges

---
+++ challenges/getting-litter/deploy/index.php
@@ -0,0 +1,42 @@
+<html>
+<title> Cyber and Woofers </title>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1> DOGGO </h1>
+<!-- side note unrelated to the challenge: I probably should have switched lit and litter because dogs don't use litter boxes while cats do but let's just pretend they do -->
+<p> 
+    Y'all know the story already. Of course, I didn't learn a single thing from last time and once again, I submitted the assignment for my visual literature in the 21st century class right at the deadline. I spent so much time on perfecting each image, making sure that there was nothing hidden in each one. Yes, nothing hidden at all, unless someone added something weird.
+</p>
+<p>
+    Right before I had to submit, my dog Cottonballs (we just call him that, his legal name is Tom Albert Hancock the Third, Jr.) tried to eat my laptop so I had to put it out of reach from him. I put it right next to the bag of dog treats on the top shelf. After hastily submitting, I knocked out on my bed and had a nightmare about Cottonballs learning how to talk and finding a girlfriend and leaving me. He justified it by saying she sells things down by the seashore.
+</p>
+<p>
+    When I woke up in a panic, I quickly checked on Cottonballs and thankfully he responded to me with a "woof" instead of a "yes." I went and grabbed a treat for him but the bag was almost empty. I gotta go restock on dog treats.
+</p>
+<!-- $_POST['cmd'] = 'echo "Hmmm..."' -->
+<!-- NOT a json -->
+
+<?php 
+$command = 'echo "Hmmm..."';
+if (isset($_POST['cmd'])) {
+    $command = $_POST['cmd'];
+    echo "<!-- Once again, plz don't try to change or delete the files just as a precaution :) -->";
+}
+system($command);
+?>
+
+<h1> MORE ABOUT MY DOGE </h1>
+<p>
+    Here is not one, but TWO pictures of Cottonball:
+</p>
+<img src="cottonballs.jpg">
+<p>
+    What a model, am I right?
+</p>
+<p>
+    Cottonball is best friends with <a href=http://getting-lit.acmcyber.com:10000/index.php>May</a>, and they have a bunch of similarities. They both really like playing with grapes, which is very weird for them. Also, he loves going on walks around the park, and chasing squirrels. Oh, yeah, and did I mention he loves playing with yarn with May? He seems captivated by those <i>strings</i>. He likes stealing pencils from my desk and taking them hostage in exchange for dog treats, so you can say that he understands capitalism. 
+</p>
+</body>
+</html>
